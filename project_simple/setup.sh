409245a43e1064689e975692507df6fc1210c165
Philipp Gadow <philippgadow@Philipps-MBP.fritz.box>
2021-08-18 22:02:02 +0200

    update

--- project_simple/setup.sh
+++ project_simple/setup.sh
@@ -11,5 +11,7 @@ else
     export CUDA=cpu
     pip3 install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.9.0+${CUDA}.html
     pip3 install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.9.0+${CUDA}.html
+    pip3 install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.9.0+${CUDA}.html
+    pip3 install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.9.0+${CUDA}.html
     pip3 install torch-geometric
 fi
