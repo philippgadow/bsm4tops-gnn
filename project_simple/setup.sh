4f6e1c47447a34b8f82848e404362cac8593cb4a
Philipp Gadow <philippgadow@Philipps-MBP.fritz.box>
2021-08-18 22:01:23 +0200

    another try for pytorch-geomatric

--- project_simple/setup.sh
+++ project_simple/setup.sh
@@ -8,4 +8,8 @@ else
     python3 -m venv venv
     source venv/bin/activate
     pip3 install -r requirements.txt
+    export CUDA=cpu
+    pip3 install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.9.0+${CUDA}.html
+    pip3 install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.9.0+${CUDA}.html
+    pip3 install torch-geometric
 fi
